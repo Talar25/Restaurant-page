9d67938d8261ed3751acf1e385b40103851d40a6
Talar25 <90799331+Talar25@users.noreply.github.com>
2023-03-07 16:07:44 +0100

    Add css variables to the root

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
-import './normalize.css';
-import './general.css';
-import './style.css';
+import './css/normalize.css';
+import './css/general.css';
+import './css/style.css';
 
 
 
-function component() {
-    const element = document.createElement('div');
+// function component() {
+//     const element = document.createElement('div');
  
-    // Lodash, now imported by this script
-    element.innerHTML = 'this is my restaurant page!'
-   element.classList.add('hello');
+//     // Lodash, now imported by this script
+//     element.innerHTML = 'this is my restaurant page!'
+//    element.classList.add('hello');
  
-    return element;
-  }
+//     return element;
+//   }
  
-  document.body.appendChild(component());
+//   document.body.appendChild(component());
