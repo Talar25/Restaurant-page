67436cf07ba02c58cf822a683f9d7c1bda016565
Talar25 <90799331+Talar25@users.noreply.github.com>
2023-03-08 14:42:33 +0100

    Add home.js with factory functions making DOM elements

---
+++ src/modules/home.js
@@ -0,0 +1,102 @@
+const createHero = function () {
+
+    //creation
+    const section = document.createElement('section');
+    section.classList.add('hero-section')
+
+    const container = document.createElement('div');
+    container.classList.add('container');
+
+    const heroBox = document.createElement('div');
+    heroBox.classList.add('hero-box');
+
+    const heroText = document.createElement('div');
+    heroText.classList.add('hero-text');
+
+    const heading = document.createElement('h2');
+    heading.classList.add('heading-secondary');
+    
+
+    heading.innerHTML = 'Welcome to the best pizzeria <span>with cats!</span>'
+
+    const div = document.createElement('div');
+
+
+    const button = document.createElement('a');
+    button.classList.add('hero-btn');
+    button.textContent = 'Look what we have &rarr;';
+
+    //appending
+    heroText.appendChild(heading);
+
+    heroBox.appendChild(heroText)
+    heroBox.appendChild(div)
+
+    container.appendChild(heroBox)
+    container.appendChild(button)
+
+    section.appendChild(container)
+
+    return section;
+}
+
+const createGallery = function (where) {
+
+    for(let i = 1; i < 7; i++) {
+        const item = document.createElement('figure');
+        item.classList.add('gallery-item');
+        const img = document.createElement('img');
+        img.src = `../src/img/cat-${i}.jpg`
+        img.alt = 'cat'
+
+        item.appendChild(img)
+
+        where.appendChild(item)
+    }
+
+}
+
+
+const createAbout = function () {
+    //creation
+    const section = document.createElement('section');
+    section.classList.add('about-section')
+
+    const container = document.createElement('div');
+    container.classList.add('container');
+
+    const aboutBox = document.createElement('div')
+    aboutBox.classList.add('about-box')
+
+    const heading = document.createElement('h3');
+    heading.classList.add('heading-tertiary');
+    heading.textContent = 'Have a look at our employes';
+
+    const aboutParagraph1 = document.createElement('p');
+    aboutParagraph1.textContent = "aren't they cute?"
+    const aboutParagraph2 = document.createElement('p');
+    aboutParagraph2.textContent = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem ex reprehenderit doloremque sint, a pariatur, iure distinctio molestiae qui, cum modi aliquam saepe omnis voluptatum quae consequatur laboriosam possimus libero!'
+
+    const gallery = document.createElement('div')
+    gallery.classList.add('gallery')
+
+    //appending
+    createGallery(gallery);
+
+    aboutBox.appendChild(heading)
+    aboutBox.appendChild(aboutParagraph1)
+    aboutBox.appendChild(aboutParagraph2)
+
+    container.appendChild(aboutBox)
+    container.appendChild(gallery)
+
+    section.appendChild(container);
+
+    return section;
+}
+
+
+const createHome = function (){
+    createHero();
+    createAbout();
+}
--- webpack.config.js
+++ webpack.config.js
@@ -13,6 +13,10 @@ module.exports = {
         use: ['style-loader', 'css-loader'],
       },
       {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
         test: /\.(woff|woff2|eot|ttf|otf)$/i,
         type: 'asset/resource',
       },
